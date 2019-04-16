d8a1d87f697436df178398be0c52f51d1247be9f
LeonardoAwad <47328699+LeonardoAwad@users.noreply.github.com>
2019-04-15 23:02:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Academia </title>
+	</head>
+
+	<body>
+		
+		<?php 
+
+?>
+		<header>
+			<div>
+				<nav>
+					<img src="img/" alt="LOGO">
+					<ul>
+						<li>
+							
+							<span>HOME</span>
+							
+						</li>
+						<li>
+						
+							<span>SOBRE</span>
+						</li>
+						<li>
+						
+							<span>EXCLUSIVO</span>
+						</li>
+						<li>
+						
+							<span>CONTATO</span>
+						</li>
+						
+						
+					</ul>
+				</nav>
+			</div>
+		</header>
+		
+		<section><!-- SOBRE O PROGRAMA OU SERVIÇOS-->
+			<ul> 
+				<li>
+					<p> Topico 1</p>
+				</li>
+				<li>
+					<p> Topico 2</p>
+				</li>
+				<li>
+					<p> Topico 3</p>
+				</li>
+				<li>
+					<p> Topico 4</p>
+				</li>
+				<li>
+					<p> Topico 5</p>
+				</li>
+				<li>
+					<p> Topico 6</p>
+				</li>
+				
+			</ul>
+			
+			<img src="img/" alt="Video de Apresentação">
+		
+		</section>
+		
+		<section><!--CORPO HUMANO-->
+			<img src="img/" alt="Imagem corpo e musculos"<>
+		
+		</section>
+		
+		
+		<section>
+			<article>
+				<article>
+					<h2>PLANO A</h2>
+					<p>informações do plano........................................................................................................................................................................................................................................................................................................................</p>
+				</article>
+				<article>
+					<h2>PLANO B</h2>
+					<p>informações do plano........................................................................................................................................................................................................................................................................................................................</p>
+				</article>
+				<article>
+					<h2>PLANO C</h2>
+					<p>informações do plano........................................................................................................................................................................................................................................................................................................................</p>
+				</article>
+				
+			
+			</article>
+		</section>
+		
+		<section>
+			<h2>SOBRE NÓS</h2>
+			<p>somos anoes curiosos ..c.kcncnefvnflnvkdnkfnvkfnvkdfnvkdnfvkdfvnfffffffffffffffffnvkfnvkf</p>
+		</section>
+		
+		<section>EQUIPE
+			<article>
+				<img src="img/" alt="Foto equipe">
+				<h3>Nome</h3>
+			</article>
+			
+			<article>
+					<img src="img/" alt="Foto equipe">
+					<h3>Nome</h3>
+			</article>
+			
+			<article>
+					<img src="img/" alt="Foto equipe">
+					<h3>Nome</h3>
+			</article>
+			
+			<article>
+					<img src="img/" alt="Foto equipe">
+					<h3>Nome</h3>
+			</article>
+			
+		</section>
+		
+		<section><!--GALERIA-->
+			
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			<img src="img/" alt="Galeria insta">
+			
+		</section>
+		
+		<select name="objetivo" id="objetivo">
+			<option value="objetivo"></option>
+			<option value="0">Ganhar massa</option>
+			<option value="1">Ganhar anão</option>
+		</select>
+		
+		
+		
+		
+		
+		
+	</body>
+</html>
